e0f9eb18843ce5d78404bf3a06b7fd41e282e8a8
Isaac Mehigan <isaacmehigan@dyn114035.shef.ac.uk>
2017-11-28 23:22:53 +0000

    Improved function to use dataframes

--- scripts/compute_stats.py
+++ scripts/compute_stats.py
@@ -11,6 +11,8 @@ Created on Tue Nov 28 21:54:12 2017
 
 import pandas as pd
 import statistics as stat
+import datetime
+
 def compute_stats(df):
     days=[]
     mean_mag_JJY=[]
@@ -21,13 +23,15 @@ def compute_stats(df):
     for group in df.groupby(df.index.day):
         DFList.append(group[1])
     for day in DFList:
-        days.append(df.index.day)
+        days.append(day.index[0].date())
         mean_mag_JJY.append(stat.mean(day['A_jjy']))
         mean_phase_JJY.append(stat.mean(day['P_jjy']))
         disp_mag_JJY.append(stat.pvariance(day['A_jjy']))
         disp_phase_JJY.append(stat.pvariance(day['P_jjy']))
-
-
+    d={'mean_mag_JJY': mean_mag_JJY,'mean_phase_JJY': mean_phase_JJY,
+       'disp_mag_JJY': disp_mag_JJY,'disp_phase_JJY': disp_phase_JJY}
+    stats_df=pd.DataFrame(data=d,index=days)
+    print(stats_df.index)
 #collumn_names=[['M_nwc','D_nwc','M_npm','D_npm','M_jji','D_jji','M_jjy','D_jjy']]
 stats_data=[]*4
 
