5446834cb5a4ce26fc9321e470c5aec5151f9f7a
Isaac Mehigan <isaacmehigan@Isaacs-MacBook-Pro-4.local>
2017-11-28 18:22:09 +0000

    Added linear regression function

--- scripts/main.py
+++ scripts/main.py
@@ -20,6 +20,7 @@ import string
 import matplotlib.pyplot as plt
 import datetime
 import numpy as np
+from scipy import stats
 
 ##Plot figures in a new window
 #from IPython import get_ipython
@@ -40,9 +41,11 @@ def filename2date(filename):
 
 def add_date_col(data,file):
     length=len(data.index)
+    tmp=data.index
     start_date=filename2date(file)
     rng=pandas.date_range(start_date,freq='20s',periods=length)
     data['time']=rng
+    data['num']=tmp
     data2=data.set_index('time')
     return data2
 
@@ -57,14 +60,13 @@ Alldata=[]
 #Alldata = pandas.DataFrame([[], []])
 data=[0]*8
 
-files=os.listdir('./eq2/')
-os.chdir('./eq2/')
+files=os.listdir('../kam/eq2/')
+os.chdir('../kam/eq2/')
 CWD=os.getcwd()
 
-start_date=files[0]
+#start_date=files[0]
 
 #for file in files:
-
 
 for file in files:
 
@@ -79,21 +81,28 @@ for file in files:
                        skiprows=1
                        )
     data=add_date_col(data,file)
+#    data['num']=list(range(0,len(Alldata.index)))
+    night_data=data.between_time('18:00','8:00')
     Alldata.append(data.iloc[0:1400,:])
     Alldata.append(data.iloc[3700:,:])
 Alldata=pandas.concat(Alldata)
-
+All_night_data=Alldata.between_time('18:00','8:00')
 #Ajji=min(data.iloc[:,4])
 plt.figure(num=None, figsize=(10, 6), dpi=200)
 
-print(filename)
-np.argmin(data.iloc[:,4])
+#print(filename)
+#np.argmin(data.iloc[:,4])
 
-N = len(Alldata.index)
-x = np.linspace(0, N, N,endpoint=False)
+#N = len(Alldata.index)
+##x = np.linspace(0, N, N,endpoint=False)
 #x = np.array([datetime.datetime(2004, 1, 9, i, 0) for i in range(24)])
+x=All_night_data['num']
+y=All_night_data['A_jji']
 
-JJIamp=plt.plot(x, Alldata.iloc[:,4], 'r',x,Alldata.iloc[:,6],'b')
+JJIamp=plt.plot(All_night_data.index[:], All_night_data.iloc[:,4], 'r',All_night_data.index[:],All_night_data.iloc[:,6],'b')
+
+[slope, intercept, r_value, p_value, std_err] = stats.linregress(x,y)
+
 #JJIphase=plt.plot(x, data.iloc[:,5], 'r--')
 #JJIamp.show()
 #JJIphase.show()
