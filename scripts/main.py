9ba9ed6563ac200e558a2787a30e183c326c9c99
Isaac Mehigan <isaacmehigan@dyn114035.shef.ac.uk>
2017-11-29 00:07:48 +0000

    Add compute_stats() fcn to main.py and other tweaks

--- scripts/main.py
+++ scripts/main.py
@@ -12,11 +12,12 @@ amplitude phase (NWC transmitter) amplitude phase (NPM) amplitude phase (JJI) am
 
 JJI is main for EQs
 """
-import pandas
+import pandas as pd
 import os
 import re
 import glob
 import string
+import statistics as stat
 import matplotlib.pyplot as plt
 import datetime
 import numpy as np
@@ -39,29 +40,50 @@ def filename2date(filename):
     date=datetime.datetime(year,mon,day)-datetime.timedelta(minutes=1)
     return(date)
 
-def add_date_col(data,file):
+def add_date_col(data,start_date):
     length=len(data.index)
     tmp=data.index
-    start_date=filename2date(file)
-    rng=pandas.date_range(start_date,freq='20s',periods=length)
+#    start_date=filename2date(file)
+    rng=pd.date_range(start_date,freq='20s',periods=length)
     data['time']=rng
     data['num']=tmp
     data2=data.set_index('time')
     return data2
 
+def compute_stats(df):
+    days=[]
+    mean_mag_JJY=[]
+    disp_mag_JJY=[]
+    mean_phase_JJY=[]
+    disp_phase_JJY=[]
+    DFList = []
+    for group in df.groupby(df.index.day):
+        DFList.append(group[1])
+    for day in DFList:
+        days.append(day.index[0].date())
+        mean_mag_JJY.append(stat.mean(day['A_jjy']))
+        mean_phase_JJY.append(stat.mean(day['P_jjy']))
+        disp_mag_JJY.append(stat.pvariance(day['A_jjy']))
+        disp_phase_JJY.append(stat.pvariance(day['P_jjy']))
+    d={'mean_mag_JJY': mean_mag_JJY,'mean_phase_JJY': mean_phase_JJY,
+       'disp_mag_JJY': disp_mag_JJY,'disp_phase_JJY': disp_phase_JJY}
+    stats_df=pd.DataFrame(data=d,index=days)
+    return stats_df
+
 #------------------------------------------------------------
 
 print("Loading the data")
-
+eq_name='eq2'
 colNames=['A_nwc','P_nwc','A_npm','P_npm','A_jji','P_jji','A_jjy','P_jjy']
 #months=['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
 #Alldata=[0]*8
 Alldata=[]
+measures=[]
 #Alldata = pandas.DataFrame([[], []])
 data=[0]*8
 
-files=os.listdir('../kam/eq2/')
-os.chdir('../kam/eq2/')
+files=os.listdir('../kam/'+eq_name)
+os.chdir('../kam/'+eq_name)
 CWD=os.getcwd()
 
 #start_date=files[0]
@@ -71,7 +93,7 @@ CWD=os.getcwd()
 for file in files:
 
     filename=os.path.join(os.getcwd(),file)
-    data = pandas.read_csv(filename,
+    data = pd.read_csv(filename,
                        sep='\s+', 
                        engine='python',
                        index_col=False,
@@ -80,15 +102,18 @@ for file in files:
                        header=None,
                        skiprows=1
                        )
-    data=add_date_col(data,file)
+    data=add_date_col(data,filename2date(file))
 #    data['num']=list(range(0,len(Alldata.index)))
     night_data=data.between_time('18:00','8:00')
-    Alldata.append(data.iloc[0:1400,:])
-    Alldata.append(data.iloc[3700:,:])
-Alldata=pandas.concat(Alldata)
+#    Alldata.append(data.iloc[0:1400,:])
+#    Alldata.append(data.iloc[3700:,:])
+    Alldata.append(data)
+Alldata=pd.concat(Alldata)
 All_night_data=Alldata.between_time('18:00','8:00')
 #Ajji=min(data.iloc[:,4])
-plt.figure(num=None, figsize=(10, 6), dpi=200)
+stats_df=compute_stats(All_night_data)
+
+plt.figure(num=2, figsize=(10, 6), dpi=200)
 
 #print(filename)
 #np.argmin(data.iloc[:,4])
