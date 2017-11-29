fdc319c2d0a738485f364e041b483b8dee3ed358
Isaac Mehigan <isaacmehigan@dyn114035.shef.ac.uk>
2017-11-29 01:30:47 +0000

    Add plotting function for the normalized dataframe

--- scripts/norm_and_plot.py
+++ scripts/norm_and_plot.py
@@ -7,9 +7,11 @@ Created on Wed Nov 29 00:09:54 2017
 """
 
 #Normalize all the values in a dataframe, keeping col names and index
+#Plot a bar chart and a line chart of the dataframe for comparison
 
 from sklearn import preprocessing
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def norm_df(df):
     x = df.values #returns a numpy array
@@ -19,3 +21,8 @@ def norm_df(df):
     column_headers=list(df)
     df = pd.DataFrame(x_scaled,columns=column_headers,index=index)
     return df
+
+def plot_stat_df(df):
+    df.plot.bar()
+    df.plot()
+    
