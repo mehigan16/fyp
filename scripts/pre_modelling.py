3f3ed59dea2f3198341d4c1d50a0bd00223a8e63
Isaac Mehigan <isaacmehigan@Isaacs-MacBook-Pro-4.local>
2018-02-20 23:44:54 +0000

    Add search for suitable control values

--- scripts/pre_modelling.py
+++ scripts/pre_modelling.py
@@ -11,61 +11,108 @@ Pre-modelling
 
 
 import dst as dst
-#import vlf as vlf
+import vlf as vlf
 #import eq as eq
-#import compute_stats as cs
+import compute_stats as cs
 import os
 import pandas as pd
 import datetime
-
+import statistics as stat
+import numpy as np
 
 def load():
+    eq_array=[] #np.zeros((2,20))
+#    result_array = np.array([])
+    result_array2 = np.zeros(20)
+    for filename in os.listdir("../h5/kam"):
+        if filename.endswith(".h5"):
+            df=pd.read_hdf("../h5/kam/"+filename)
+            stats_df=cs.compute_stats(df)
+            stats_list=stats_df.values.flatten()[0:20]
+#            eq_array=np.append(eq_array,stats_list,axis=1)
+#            eq_array.append(stats_list)
+#            result_array = np.append(result_array, stats_list)
+            result_array2= np.vstack((result_array2, stats_list))
+#            i=i+1
+#            eq_array=[eq_array;stats_list]
+    return result_array2
+    
     print("done")
+
+def select_control_plot(df,eq_df): 
     
-def test_date(df,date,*threshold):
-    """ Function to check if the dst value was below the threshold on the
-    5 days leading to the given datetime. threshold is -50 if nothing given.
-    """
     
-    D=df.iloc[df.index.get_loc(date,method='nearest')]
-    date=D.name
-    lower_date=date-datetime.timedelta(days=5)
-    upper_date=date +datetime.timedelta(hours=1)
-    exact_value=df.loc[date].dst
-    values=df.loc[lower_date:upper_date].dst
-    minimum=values.min()
-    average=values.mean()
-    if not threshold:
-        threshold = -50
-    else:
-        threshold=threshold[0]
-    if minimum > threshold:
-        return 1
-#    elif (exact_value > threshold and average > threshold):
-##        print("Warning at least 1 value in the time range was below threshold")
-#        return 1
-    else:
-        return 0
-
-
-
-
+#    dst_y=df2['dst']
+    
+    df2=df['dst']>-50
+    
+    df['eq']=np.zeros(dst_df.shape[0])
+    
+    for index, row in eq_df.iterrows():
+        eq_date=datetime.datetime(index.year,index.month,index.day)
+        
+        D=df.iloc[df.index.get_loc(eq_date,method='nearest')]
+        D2=df.index.get_loc(eq_date,method='nearest')
+        
+#        df.iloc[D.name][2]=1
+        for i in range(1,120):
+            idx=df.index[D2-i]       
+            df=df.set_value([idx],['eq'],1)
+        df3=df[(df['dst']>(-50)) & (df['eq'] == 0)]
+        df4=df3.between_time('00:00','00:30')
+        
+        
+        
+        
+        
+        
+#    print("done")
+#    for index, row in df2.iterrows():
+#        if df2['dst']:
+#            df2['marker']='g'
+#        else:
+#            df2['marker']='r'
+#    
+#    start_date=datetime.datetime(2004,2,10)
+#    rng=pd.date_range(start_date,freq='1d',periods=1421)
+    
+    return df4
 
 dst_df=pd.read_hdf("../h5/dst.h5")
 eq_df=pd.read_hdf("../h5/eq.h5")
-    
+
+
+decision_df=select_control_plot(dst_df,eq_df)
+#eq_array=load()
+
+dst_mean=stat.mean(dst_df.dst)
+dst_std=stat.pstdev(dst_df.dst)
+
+threshold=dst_mean-(2*dst_std)
+
+   
 eq_df_candidates=eq_df    
 for index, eq in eq_df.iterrows():
-    index.date    
-    if test_date(dst_df,index):
-#        print("Earthquake at " + str(index) + " is a possible candidate")
-    else:
-        eq_df_candidates=eq_df_candidates.drop(index)
+    eq_date=datetime.datetime(index.year,index.month,index.day)
     
+    if not dst.test_date(dst_df,index,threshold):      
+        eq_df_candidates=eq_df_candidates.drop(index)
+ 
+# Need to select most powerful earthquake on given day
+
+       
+#for index, eq in eq_df_candidates.iterrows():
+#    
+#    eq_date=datetime.datetime(index.year,index.month,index.day)
+#    vlf.create_hdf5(-1,eq_date)
+    
+    
+    
+    
+#print("Earthquake at " + str(index) + " is a possible candidate")
     
 #    for file in os.listdir("../h5"):
 #        df=pd.read_hdf("../h5/"+file,file.split('.')[0])
 #        print("done")
         
         
-#load()
