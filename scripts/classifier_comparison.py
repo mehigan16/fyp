0af2e51e201fd388299a93652185c6aa1bf34267
Isaac Mehigan <isaacmehigan@dyn070048.shef.ac.uk>
2018-03-14 20:35:44 +0000

    Plot average ROC curves of all classifiers looked at

--- scripts/classifier_comparison.py
+++ scripts/classifier_comparison.py
@@ -10,9 +10,14 @@ Edited classifier comparison
 """
 
 import numpy as np
+
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+
+from scipy import interp
+
 from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
@@ -25,11 +30,12 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
+names = ["Logistic Regression","Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
          "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
-         "Naive Bayes", "QDA"]
+         "Naive Bayes"] #, "QDA"]
 
 classifiers = [
+    LogisticRegression(C=1e5),
     KNeighborsClassifier(3),
     SVC(kernel="linear", C=0.025,probability=True),
     SVC(gamma=2, C=1,probability=True),
@@ -38,8 +44,8 @@ classifiers = [
     RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
     MLPClassifier(alpha=1,max_iter=500),
     AdaBoostClassifier(),
-    GaussianNB(),
-    QuadraticDiscriminantAnalysis()]
+    GaussianNB()]#,
+    #QuadraticDiscriminantAnalysis()]
 
 D=np.loadtxt("earthquake_data.csv", delimiter=",")
 
@@ -51,17 +57,97 @@ X=D_scaled[:,:-1]
 
 Z=D_scaled[:,-1]
 
-X_train, X_test, y_train, y_test =   train_test_split(X,Z,test_size=0.50)
-
+#np.vstack((result_array2, stats_list))
 results={}
+#i=0
 
-for name, clf in zip(names, classifiers):
-        clf.fit(X_train, y_train)
-        score = clf.score(X_test, y_test)
-        if hasattr(clf, "predict_proba"):
-            y_star=clf.predict_proba(X_test)[:,1]
-            [fpr,tpr,thresholds]=metrics.roc_curve(y_test, y_star, pos_label=None, sample_weight=None, drop_intermediate=True)
-            auc=metrics.roc_auc_score(y_test, y_star)
-            
-            results[name]=auc
+
+#auc_list=[]
+#fpr_list=[]
+#tpr_list=[]
+#result_array = np.empty((0, 100))
+def compare_classifiers(n):
+    auc_array=np.array([]).reshape(0,10)
+    fpr_array=np.array([]).reshape(0,10)
+    tpr_array=np.array([]).reshape(0,10)
+    for j in range(0,n):
+        auc_list=[]
+        fpr_list=[]
+        tpr_list=[]
+        X_train, X_test, y_train, y_test =   train_test_split(X,Z,test_size=0.50)
+        for name, clf in zip(names, classifiers):
+                clf.fit(X_train, y_train)
+#                score = clf.score(X_test, y_test)
+                if hasattr(clf, "predict_proba"):
+                    y_star=clf.predict_proba(X_test)[:,1]
+                    [fpr,tpr,thresholds]=metrics.roc_curve(y_test, y_star, pos_label=None, sample_weight=None, drop_intermediate=True)
+                    auc=metrics.roc_auc_score(y_test, y_star)
+    #                results[name]=auc
+                    auc_list.append(auc)
+                    fpr_list.append(fpr)
+                    tpr_list.append(tpr)
+#                    i=i+1
+        auc_array=np.vstack([auc_array,auc_list])
+        fpr_array=np.vstack([fpr_array,fpr_list])
+        tpr_array=np.vstack([tpr_array,tpr_list])
+
+def plot_curves(tpr_array,fpr_array,auc_array):
         
+        base_fpr = np.linspace(0, 1, 101)
+        plt.figure(figsize=(27, 9))
+        
+        for i in range(0,10):
+            tprs = []
+            for run in range(0,len(auc_array)):
+                ax=plt.subplot(3,4,i+1)
+                fpr=fpr_array[run,i]
+                tpr=tpr_array[run,i]
+                ax.plot(fpr, tpr, 'b', alpha=0.15)
+                tpr = interp(base_fpr, fpr, tpr)
+                tpr[0] = 0.0
+                tprs.append(tpr)   
+            tprs = np.array(tprs)
+            mean_tprs = tprs.mean(axis=0)
+            std = tprs.std(axis=0)
+            tprs_upper = np.minimum(mean_tprs + std, 1)
+            tprs_lower = mean_tprs - std
+            auc_mean=np.average(auc_array[:,i])
+            
+            ax.plot(base_fpr, mean_tprs, 'b')
+            ax.fill_between(base_fpr, tprs_lower, tprs_upper, color='grey', alpha=0.3)
+            
+            ax.plot([0, 1], [0, 1],'r--',label='Mean AUC = %0.2f'% auc_mean)
+            ax.legend(loc='lower right')
+            ax.set_xlim([-0.01, 1.01])
+            ax.set_ylim([-0.01, 1.01])
+            ax.set_ylabel('True Positive Rate')
+            ax.set_xlabel('False Positive Rate')
+            ax.set_title(names[i])
+#            ax.set_aspect('equal', 'datalim')
+            
+        plt.tight_layout()
+        plt.show()
+
+        
+        
+        
+plot_curves(tpr_array,fpr_array,auc_array)
+    
+#fpr_lengths=np.array([]).reshape(0,10)
+#tpr_lengths=np.array([]).reshape(0,10)
+#for l in range(0,20):
+#    tmp_list=[]
+#    tmp_list2=[]
+#    for k in range(0,10):
+#        tmp_list.append(len(fpr_array[l,k]))
+#        tmp_list2.append(len(tpr_array[l,k]))
+#    fpr_lengths=np.vstack([fpr_lengths,tmp_list])
+#    tpr_lengths=np.vstack([tpr_lengths,tmp_list2])
+    
+    
+    
+    
+    
+    
+    
+    
