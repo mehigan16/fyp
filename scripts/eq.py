dd51a0979cf41142cfe1a3e0c445f0c3b8322b8c
Isaac Mehigan <isaacmehigan@Isaacs-MacBook-Pro-4.local>
2018-04-10 14:59:57 +0100

    Load all eqs, and add tectonic plates to map

--- scripts/eq.py
+++ scripts/eq.py
@@ -12,6 +12,8 @@ import os
 import datetime
 import pandas as pd
 from mpl_toolkits.basemap import Basemap
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
 import matplotlib.pyplot as plt
 
 def load(file):
@@ -45,12 +47,14 @@ def convert_date_time(df):
 def get_marker_color(magnitude):
     """ Function to change marker size and colour for the plotting of eqs based 
     on the magnitude of the eq"""
-    if magnitude < 6.0:
-        return ('go'),6
+    if magnitude < 5.5:
+        return ('go'),2
+    elif magnitude < 6.0:
+        return ('bo'),5
     elif magnitude < 6.5:
-        return ('yo'),8
+        return ('yo'),7
     else:
-        return ('ro'),10
+        return ('ro'),9
 
 def check_valid(eq,distance_threshold):
     """ Function to check if the earthquake would have been registered along the
@@ -61,6 +65,8 @@ def check_valid(eq,distance_threshold):
     tLat=37.372557
     rLong=158.92
     rLat=53.15
+    if eq.mag < 4:
+        return -1
     if eq.latitude > rLat:
         return -1
     elif eq.latitude<tLat:
@@ -68,7 +74,7 @@ def check_valid(eq,distance_threshold):
     if eq.longitude>rLong:
         return -1
     elif eq.longitude<tLong:
-        return -1   
+        return -1
     v=[rLong-tLong, -(rLat-tLat)] # Point A is [x1,y1] Point B is [x2,y2]
     u=[tLong-eq.longitude, tLat-eq.latitude]
     distance = abs( (rLong-tLong)*(tLat-eq.latitude) - (tLong-eq.longitude)*(rLat-tLat) )
@@ -112,11 +118,15 @@ def plot(df):
     map.drawcountries()
     map.fillcontinents(color = 'gray')  #coral
     map.drawmapboundary()
-
+    map.readshapefile('/Users/isaacmehigan/Documents/fyp/eq_lists/plates/PB2002_plates', 
+                name='tectonic_plates', 
+                drawbounds=True, 
+                color='red')
 
     linex,liney = map([tLong,rLong],[tLat, rLat])
     map.plot(linex,liney,'m')
     map.plot(linex,liney,'m*',markersize=15)
+    
     
     lons = df.longitude.tolist()
     lats = df.latitude.tolist()
@@ -128,18 +138,29 @@ def plot(df):
 #    data2=data
     
     for index, row in df.iterrows():         
-        map.plot(row.x, row.y, row.marker , markersize=row.marker_size )
+        map.plot(row.x, row.y, row.marker , markersize=row.marker_size, alpha=0.1*row.marker_size)
 
+#    Proxy labels
+    plate_lines = mlines.Line2D([], [], color='red',label='Tectonic plate boundary')
+    green_circle = mpatches.Circle([], radius=1, color='g',  alpha=0.2, label='$M < 5.5$')
+    blue_circle = mpatches.Circle([], radius=2, color='b',  alpha=0.5, label='$5.5 < M < 6$')
+    yellow_circle = mpatches.Circle([], radius=4, color='y',  alpha=0.7, label='$6 < M < 6.5$')
+    red_circle = mpatches.Circle([], radius=5, color='r',  alpha=0.9, label='$M > 6.5$')
+    
+    plt.legend(handles=[plate_lines,green_circle,blue_circle,yellow_circle,red_circle])
+    
     plt.show()
 
-
+def save(df):
+    df.to_hdf('../h5/eq.h5', 'eq', format = 'table', mode='w')
 
 # Section for testing the functions within the file.
 distance_threshold=50
 
-df=load("2004_2007_eqs.csv")
-
-df=convert_date_time(df)
-
-df2=proccess_eqs(df,distance_threshold)
+#df=load("2004_2007_all_small-big_eqs.csv")
+#
+#df=convert_date_time(df)
+#
+#df2=proccess_eqs(df,distance_threshold)
 plot(df2)
+#save(df2) # Save the eq data
