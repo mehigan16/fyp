f52161d2d5e53579d3463633edc72bca4081b5f9
Isaac Mehigan <isaacmehigan@dyn070033.shef.ac.uk>
2018-04-13 14:28:20 +0100

    Load and save flux data for timespan

--- scripts/flux.py
+++ scripts/flux.py
@@ -19,19 +19,35 @@ import requests
 def load(file):
     """ Function to load the dst data into a dataframe
     """
-#    colNames=['DST_name','Version','Base_value','1','2','3','4','5','6','7','8','9','10',
-#          '11','12','13','14','15','16','17','18','19','20','21','22','23','24','avg']
+#    
+    data_start=find_data_start(file,"data:")
     flux_data = pd.read_csv(file,
                        sep=',', 
                        engine='python',
-                       index_col=False,
-                       header=None,
-                       skiprows=215
+                       index_col='time_tag',
+                       skiprows=data_start
                        )
 
 #    flux_data=add_date_col(dst_data)
     return flux_data
 
+
+def load_all(directory):
+    df = pd.DataFrame() #creates a new dataframe that's empty
+#    newDF = newDF.append(oldDF, ignore_index = True)
+    files=sorted(os.listdir(directory))
+    for file in files:
+        if file.lower().endswith('.csv') and file.startswith('200'):
+            month_df=load(directory+file)
+            df=df.append(month_df)
+    return df
+    
+def find_data_start(filepath, keywords):
+   with open(filepath) as f:
+       for counter, line in enumerate(f, start = 1):
+           if line.find(keywords) >= 0:
+              return counter   
+    
 
 def download(output_dir):
     already_downloaded_files=os.listdir(output_dir)
@@ -53,8 +69,19 @@ def download(output_dir):
                 counter2=counter2+1
 #                print(output_filename + " downloaded")
     print(str(counter2) + " files downloaded " + str(counter1) + " files were already present")
-#flux_data=load("/Users/isaacmehigan/Documents/fyp/flux/data/g10_eps_1m_20040101_20040131.csv")
-download("/Users/isaacmehigan/Documents/fyp/flux/data/")
+
+
+def save(df):
+    """ Function to save data""" 
+    df.to_hdf('/Users/isaacmehigan/Documents/fyp/h5/flux.h5', 'flux', format = 'table', mode='w')
+
+
+#flux_data=load("/Users/isaacmehigan/Documents/fyp/flux/data/2004-01-flux.csv")
+#file=open("/Users/isaacmehigan/Documents/fyp/flux/data/2004-01-flux.csv")
+
+#flux_df=load_all("/Users/isaacmehigan/Documents/fyp/flux/data/")
+save(flux_df[['e1_flux_i','p1_flux']])
+#download("/Users/isaacmehigan/Documents/fyp/flux/data/")
 #year=2004
 #month=1
 #generic='https://satdat.ngdc.noaa.gov/sem/goes/data/avg/YEAR/MON/goes10/csv/'
